6f9cef0a33e47b7b9ab0f5ac92fb5f1ff2690776
parthChittoraItt <parth.chittora@intimetec.com>
2025-08-14 14:21:49 +0530

    Code divided into smaller functions and maeaningful names are given to the variables.

--- assignment/Week1/pattern.cpp
+++ assignment/Week1/pattern.cpp
@@ -17,84 +17,103 @@ void printSpaces(ll count)
         cout << " ";
     }
 }
+void printUpperPortion(ll count, ll totalRowsToPrint)
+{
+    for (ll row = 0; row <= count; row++)
+    {
+        if (count % 2 == 1)
+        {
+            printAlternateBits(row + 1);
+            printSpaces(2 * (count - row));
+            printAlternateBits(row + 1);
+            cout << endl;
+        }
+        else if (count % 2 == 0)
+        {
+            if (row == count)
+            {
+                printAlternateBits(totalRowsToPrint);
+            }
+            else
+            {
+                printAlternateBits(row + 1);
+                printSpaces(2 * (count - row) - 1);
+                printAlternateBits(row + 1);
+            }
+            cout << endl;
+        }
+    }
+}
+void printLowerPortion(ll count, ll totalRowsToPrint)
+{
+    for (ll row = count + 1; row < totalRowsToPrint; row++)
+    {
+        if (count % 2 == 1)
+        {
+            printAlternateBits(totalRowsToPrint - row);
+            printSpaces(2 * (row - count));
+            printAlternateBits(totalRowsToPrint - row);
+            cout << endl;
+        }
+        else if (count % 2 == 0)
+        {
+            printAlternateBits(totalRowsToPrint - row);
+            printSpaces(2 * (row - count) - 1);
+            printAlternateBits(totalRowsToPrint - row);
+            cout << endl;
+        }
+    }
+}
+void printPattern()
+{   cin.ignore(1000 , '\n');
+    ll count{};
+    cout << "Enter the value of count (Poistive Integer): ";
+    cin >> count;
+    cin.ignore();
+    if (cin.fail())
+    {
+        cin.clear();
+        cin.ignore(1000, '\n');
+    }
+    ll totalRowsToPrint = count * 2 + 1;
+    if (count >= 0)
+    {
+        printUpperPortion(count, totalRowsToPrint);
+        printLowerPortion(count, totalRowsToPrint);
+    }
+    else
+    {
+        cout << "Please enter a positive integer." << endl;
+    }
+}
 
 int main()
 {
-    ll count;
     while (true)
     {
+        
         cout << "\n1. Print Pattern" << endl;
         cout << "2. Exit" << endl;
         cout << "Enter choice :";
         int choice;
         cin >> choice;
-
-        if (cin.fail()) // checks if the input failed
+        if (cin.fail())
         {
-            cin.clear();            // Clear error flag
-            cin.ignore(1000, '\n'); // Discard invalid input
-            cout << "Enter a valid choice.\n";
+            cin.clear();
+            cin.ignore(1000,'\n');
+            cout << "Enter integer only." << endl;
         }
-
         if (choice == 1)
-        {   cin.ignore(1000,'\n');
-            cout << "Enter the value of count (Poistive Integer): ";
-            cin >> count;
-            ll realCount = count * 2 + 1; // total rows to be printed
-            if (count >= 0)
-            { // printing upper portion
-                for (ll i = 0; i <= count; i++)
-                {
-                    if (count % 2 == 1) // for odd count
-                    {
-                        printAlternateBits(i + 1);
-                        printSpaces(2 * (count - i));
-                        printAlternateBits(i + 1);
-                        cout << endl;
-                    }
-                    else if (count % 2 == 0) // for even
-                    {
-                        if (i == count)
-                        {
-                            printAlternateBits(realCount);
-                        }
-                        else
-                        {
-                            printAlternateBits(i + 1);
-                            printSpaces(2 * (count - i) - 1);
-                            printAlternateBits(i + 1);
-                        }
-                        cout << endl;
-                    }
-                }
-                // lower part
-                for (ll i = count + 1; i < realCount; i++)
-                {
-                    if (count % 2 == 1)
-                    {
-                        printAlternateBits(realCount - i);
-                        printSpaces(2 * (i - count));
-                        printAlternateBits(realCount - i);
-                        cout << endl;
-                    }
-                    else if (count % 2 == 0)
-                    {
-                        printAlternateBits(realCount - i);
-                        printSpaces(2 * (i - count) - 1);
-                        printAlternateBits(realCount - i);
-                        cout << endl;
-                    }
-                }
-            }
-            else
-            {
-                cout << "Please enter a positive integer." << endl;
-            }
+        {
+            printPattern();
         }
         else if (choice == 2)
+        {
             break;
-        else cout<<"Enter a valid choice.\n";
-        cin.ignore(100 ,'\n');
+        }
+        else
+            cout << "Enter a valid choice.";
+        cin.ignore(1000 , '\n');
     }
     return 0;
 }
