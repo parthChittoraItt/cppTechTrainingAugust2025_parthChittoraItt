bd0096855e32edd0f00e99ba2b491c3b462f6a46
parthChittoraItt <parth.chittora@intimetec.com>
2025-08-20 14:11:51 +0530

    static linking example

---
+++ practice/Week2/20-08-2025/addMul.h
@@ -0,0 +1,5 @@
+#ifndef ADD_MUL_H
+#define ADD_MUL_H
+int add(int a,int b);
+int mul(int a ,int b);
+#endif
---
+++ practice/Week2/20-08-2025/addMulMain.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "addMul.h"
+int main()
+{
+    std::cout << "On adding :" << add(3, 4) << std::endl;
+    std::cout << "On Multiplying :" << mul(3, 4) << std::endl;
+    return 0;
+}
