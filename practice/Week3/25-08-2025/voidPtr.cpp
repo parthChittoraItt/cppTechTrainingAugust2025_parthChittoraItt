c8e8a905fdf008f21f89dbbfadf9a543cebb5222
parthChittoraItt <parth.chittora@intimetec.com>
2025-08-31 13:16:53 +0530

    Week 3 files modified

--- practice/Week3/25-08-2025/voidPtr.cpp
+++ practice/Week3/25-08-2025/voidPtr.cpp
@@ -6,5 +6,6 @@ std::cout<< p << " " << *p<<std::endl;
 void * p0 = p;
 std::cout<<p0;      //<<" "<<*p0; cant dereference a void ptr as it does not know how many bytes to read
                     // this is the same reason why we cant perform arithmetic operations on it .
+// std::cout<<*p0;
 return 0;
 }
