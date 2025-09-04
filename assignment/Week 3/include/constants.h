640bba7fe4e65b5ea64be4b4f714aabbbc705a9e
parthChittoraItt <parth.chittora@intimetec.com>
2025-09-04 11:49:58 +0530

    Constant file cpp file deleted and handled in .h file

--- assignment/Week 3/include/constants.h
+++ assignment/Week 3/include/constants.h
@@ -1,25 +1,25 @@
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
-extern const char* enterCol ;
-extern const char* inputForMatrix1 ;
-extern const char* inputForMatrix2 ;
-extern const char* additionDimensionMismatch ;
-extern const char* multiplicationDimensionMismatch;
-extern const char* invalidChoice ;
-extern const char* onMultiplying ;
-extern const char* enterRow ;
-extern const char* onAdding ;
-extern const char* positiveOnly;
-extern const char* numberOnly ;
-extern const char* choice1 ;
-extern const char* choice2;
-extern const char* choice3 ;
-extern const char* enterChoice ;
-extern const char* oneSpace;
-extern const char* newLine;
-extern const char* enterValueMatrix1 ;
-extern const char* enterValueMatrix2;
-extern const char* invertedSquareBrackets ;
-extern const char* closedSquareBracket;
-extern const char* enterValue ;
+#ifndef  CONSTANTS_H
+#define  CONSTANTS_H
+static const char* enterRow = "Enter number of rows: ";
+static const char* enterCol = "Enter number of columns: ";
+static const char* inputForMatrix1 = "For Matrix 1:\n";
+static const char* inputForMatrix2 = "For Matrix 2:\n";
+static const char* additionDimensionMismatch = "\nAddition not possible due to dimension mismatch.\n";
+static const char* multiplicationDimensionMismatch="\nAddition not possible due to dimension mismatch.\n";
+static const char* invalidChoice = "Invalid choice.\n";
+static const char* onMultiplying = "\nOn multiplying:\n";
+static const char* onAdding = "\nOn adding:\n";
+static const char* positiveOnly = "Please enter a positive number: ";
+static const char* numberOnly = "Please enter a positive number: ";
+static const char* choice1 = "\n1. Add Matrices\n";
+static const char* choice2 = "2. Multiply Matrices\n";
+static const char* choice3 = "3. Exit\n";
+static const char* enterChoice = "Enter your choice: ";
+static const char* oneSpace = " ";
+static const char* newLine = "\n";
+static const char* enterValueMatrix1 = "\nEnter values for Matrix 1:\n";
+static const char* enterValueMatrix2= "\nEnter values for Matrix 2:\n";
+static const char* invertedSquareBrackets = "] [";
+static const char* closedSquareBracket = "] : ";
+static const char* enterValue = "Enter value for matrix[";
 #endif
