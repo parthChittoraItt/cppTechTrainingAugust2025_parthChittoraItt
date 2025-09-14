de61df8e443fe9641238d7d4e7c04ef5edc5a9a7
parthChittoraItt <parth.chittora@intimetec.com>
2025-09-14 17:40:44 +0530

    Practice code pushed

--- practice/Week5/11-09-2025/ExplicitConstructor.cpp
+++ practice/Week5/11-09-2025/ExplicitConstructor.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 
-class PhoneNumber {
+class PhoneNumber
+{
 private:
     long number;
+
 public:
-    explicit PhoneNumber(long n) : number(n) {
+    explicit PhoneNumber(long n) : number(n)
+    {
         std::cout << "Explicit constructor called." << std::endl;
     }
 
-    void dial() const {
+    void dial() const
+    {
         std::cout << "Dialing safely: " << number << std::endl;
     }
 };
 
-void makeCall(PhoneNumber p) {
+void makeCall(PhoneNumber p)
+{
     p.dial();
 }
 
-int main() {
+int main()
+{
     // PhoneNumber home = 1234567890L; // Implicit conversion not possible.
     PhoneNumber home(1234567890);
-// Explicitly type casting
+    // Explicitly type casting
     makeCall(static_cast<PhoneNumber>(9876543210));
     makeCall(PhoneNumber{5551234567});
 }
